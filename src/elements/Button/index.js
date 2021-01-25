36f304eebf2c4910afbab4e8c5826c62fb76e0aa
dwinovic <nd.cahya19@gmail.com>
2021-01-25 08:02:34 +0700

    finish component parts/mostpicked + categories

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -1,69 +1,71 @@
-import React from 'react'; 
-import { Link } from 'react-router-dom'; 
-import propTypes from 'prop-types';
+import React from "react";
+import { Link } from "react-router-dom";
+import propTypes from "prop-types";
 
-export default function Button(props) { 
-  const className = [props.className]
-  if(props.isPrimary) className.push("btn-primary")
-  if(props.isLarge) className.push("btn-lg")
-  if(props.isSmall) className.push("btn-sm")
-  if(props.isBlock) className.push("btn-block");
-  if(props.hasShadow) className.push("btn-shadow")
+export default function Button(props) {
+  const className = [props.className];
+  if (props.isPrimary) className.push("btn-primary");
+  if (props.isLight) className.push("btn-light");
+  if (props.isLarge) className.push("btn-lg");
+  if (props.isSmall) className.push("btn-sm");
+  if (props.isBlock) className.push("btn-block");
+  if (props.hasShadow) className.push("btn-shadow");
 
   const onClick = () => {
-    if(props.onClick) props.onclick();
+    if (props.onClick) props.onClick();
   };
 
   if (props.isDisabled || props.isLoading) {
     if (props.isDisabled) className.push("disabled");
     return (
-      <span className={className.join(" ")}
-      style={props.style}>
-      {props.isLoading ? (
-        <>
-          <span className="spinner-border spinner-border-sm mx-5"></span>
-          <span className="sr-only">Loading...</span>
-        </>
-      ) : ( 
-        props.children
-      )} </span>
+      <span className={className.join(" ")} style={props.style}>
+        {props.isLoading ? (
+          <>
+            <span className="spinner-border spinner-border-sm mx-5"></span>
+            <span className="sr-only">Loading...</span>
+          </>
+        ) : (
+          props.children
+        )}
+      </span>
     );
   }
 
   if (props.type === "link") {
     if (props.isExternal) {
       return (
-        <a 
+        <a
           href={props.href}
           className={className.join(" ")}
-          style= {props.style}
-          target = {props.target === "_blank" ? "_blank" : undefined}
+          style={props.style}
+          target={props.target === "_blank" ? "_blank" : undefined}
           rel={props.target === "_blank" ? "noopener noreferrer" : undefined}
-        > 
+        >
           {props.children}
         </a>
       );
     } else {
-    return(
-      <Link to={props.href}
-        className={className.join(" ")}
-        style={props.style}
-        onClick={onClick}
-      >
-        {props.children}
-      </Link>
-    );
-  } 
-}
+      return (
+        <Link
+          to={props.href}
+          className={className.join(" ")}
+          style={props.style}
+          onClick={onClick}
+        >
+          {props.children}
+        </Link>
+      );
+    }
+  }
 
-  return(
-    <Button 
+  return (
+    <button
       className={className.join(" ")}
-      style= {props.style} 
+      style={props.style}
       onClick={onClick}
     >
-        {props.children}
-    </Button>
+      {props.children}
+    </button>
   );
 }
 
@@ -81,5 +83,5 @@ Button.propTypes = {
   isSmall: propTypes.bool,
   isLarge: propTypes.bool,
   isBlock: propTypes.bool,
-  hasShadow: propTypes.bool
-}
+  hasShadow: propTypes.bool,
+};
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
-import Header from 'parts/Header'
+import landingPage from '../json/landingPage.json';
+import Header from 'parts/Header';
+import Hero from 'parts/Hero';
+import MostPicked from 'parts/MostPicked';
+import Categories from 'parts/Categories';
 
-export default class LandingPage extends Component {
-    render() {
-      return(
-        <>
-          <Header {...this.props}></Header>
-        </>
-      )
-    }
+
+export default class LandingPage extends Component { 
+  constructor() {
+    super();
+    this.refMostPicked = React.createRef();
+  }
+
+  render() {
+    return(
+      <>
+        <Header {...this.props}></Header>
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero}/> 
+        <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
+        <Categories data={landingPage.categories} /> 
+      </> 
+    );
+  }
 }
