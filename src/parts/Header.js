7a73caa0b91d7eb7780cf4a27515b687546cd255
dwinovic <nd.cahya19@gmail.com>
2021-01-26 13:17:48 +0700

    adding react-reveal and bit transition each section

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,38 +1,39 @@
-import React from 'react'; 
-import BrandIcon from 'parts/iconText'
+import React from 'react';
+import BrandIcon from 'parts/iconText';
 import Button from 'elements/Button';
+import Fade from 'react-reveal/Fade';
 
-export default function Header(props) { 
-
+export default function Header(props) {
   //Function untuk path url active
-  const getNavLinkClass = path => {
-    return props.location.pathname === path ? " active" : "";
-  }
+  const getNavLinkClass = (path) => {
+    return props.location.pathname === path ? ' active' : '';
+  };
 
-    return ( 
-      <header className="spacing-sm"> 
+  return (
+    <Fade>
+      <header className="spacing-sm">
         <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light"> 
-            <BrandIcon /> 
+          <nav className="navbar navbar-expand-lg navbar-light">
+            <BrandIcon />
             <div className="collapse navbar-collapse">
               <ul className="navbar-nav ml-auto">
-                <li className={`nav-item ${getNavLinkClass("/")}`}>
-                  <Button className="nav-link" type="link" href="/"> 
+                <li className={`nav-item ${getNavLinkClass('/')}`}>
+                  <Button className="nav-link" type="link" href="/">
                     Home
                   </Button>
                 </li>
-                <li className={`nav-item ${getNavLinkClass("/browse-by")}`}>
-                  <Button className="nav-link" type="link" href="/browse-by"> 
+                <li className={`nav-item ${getNavLinkClass('/browse-by')}`}>
+                  <Button className="nav-link" type="link" href="/browse-by">
                     Browse By
                   </Button>
                 </li>
-                <li className={`nav-item ${getNavLinkClass("/stories")}`}>
-                  <Button className="nav-link" type="link" href="/stories"> 
+                <li className={`nav-item ${getNavLinkClass('/stories')}`}>
+                  <Button className="nav-link" type="link" href="/stories">
                     Stories
                   </Button>
                 </li>
-                <li className={`nav-item ${getNavLinkClass("/agents")}`}>
-                  <Button className="nav-link" type="link" href="/agents"> 
+                <li className={`nav-item ${getNavLinkClass('/agents')}`}>
+                  <Button className="nav-link" type="link" href="/agents">
                     Agents
                   </Button>
                 </li>
@@ -41,5 +42,6 @@ export default function Header(props) {
           </nav>
         </div>
       </header>
-    )
+    </Fade>
+  );
 }
--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -4,58 +4,94 @@ import ImageHero from 'assets/images/img-hero.jpg';
 import ImageHeroFrame from 'assets/images/img-hero-frame.jpg';
 import IconCities from 'assets/images/icon/ic_cities.svg';
 import IconTravelers from 'assets/images/icon/ic_traveler.svg';
-import IconTreasure from 'assets/images/icon/ic_treasure.svg'; 
-import Button from 'elements/Button/index'; 
+import IconTreasure from 'assets/images/icon/ic_treasure.svg';
+import Button from 'elements/Button/index';
 import FormatNumber from 'utils/formatNumber';
+import Fade from 'react-reveal/Fade';
 
-export default function Hero(props) { 
+export default function Hero(props) {
   function showMostPicked() {
     window.scrollTo({
       top: props.refMostPicked.current.offsetTop - 30,
-      behavior: 'smooth'
-    })
+      behavior: 'smooth',
+    });
   }
 
   return (
-    <section className="container pt-5"> 
-      <div className="row align-items-center"> 
-        <div className="col-auto pr-5" style={{ width: 500 }}> 
-          <h1 className="font-weight-bold line-height-1 mb-3">
-            Forget Busy Work, <br/>
-            Start Next Vacation
-          </h1>
-          <p className="mb-4 font-wight-light text-gray-500 w-75" style={{ lineHeight: "170%" }}>
-              We provide what you need to enjoy your holiday with family.
-              Time to make another memorable moments.
-              Your money can return but your time is not.
-          </p>
-          <Button className="btn" hasShadow isPrimary onClick={showMostPicked}>
-            Show Me Now
-          </Button> 
+    <Fade bottom>
+      <section className="container pt-5">
+        <div className="row align-items-center">
+          <div className="col-auto pr-5" style={{ width: 500 }}>
+            <h1 className="font-weight-bold line-height-1 mb-3">
+              Forget Busy Work, <br />
+              Start Next Vacation
+            </h1>
+            <p
+              className="mb-4 font-wight-light text-gray-500 w-75"
+              style={{ lineHeight: '170%' }}
+            >
+              We provide what you need to enjoy your holiday with family. Time
+              to make another memorable moments. Your money can return but your
+              time is not.
+            </p>
+            <Button
+              className="btn"
+              hasShadow
+              isPrimary
+              onClick={showMostPicked}
+            >
+              Show Me Now
+            </Button>
 
-          <div className="row" style={{ marginTop: 80}}>
-            <div className="col-auto" style={ {marginRight: 35}}>
-              <img src={IconTravelers} alt={props.data.travelers}/>
-              <h6 className="mt-3">{FormatNumber(props.data.travelers)} <span className="text-gray-500 font-weight-light">travelers</span></h6>
+            <div className="row" style={{ marginTop: 80 }}>
+              <div className="col-auto" style={{ marginRight: 35 }}>
+                <img src={IconTravelers} alt={props.data.travelers} />
+                <h6 className="mt-3">
+                  {FormatNumber(props.data.travelers)}{' '}
+                  <span className="text-gray-500 font-weight-light">
+                    travelers
+                  </span>
+                </h6>
+              </div>
+              <div className="col-auto" style={{ marginRight: 35 }}>
+                <img src={IconTreasure} alt={props.data.treasures} />
+                <h6 className="mt-3">
+                  {FormatNumber(props.data.treasures)}{' '}
+                  <span className="text-gray-500 font-weight-light">
+                    treasure
+                  </span>
+                </h6>
+              </div>
+              <div className="col-auto">
+                <img src={IconCities} alt={props.data.cities} />
+                <h6 className="mt-3">
+                  {FormatNumber(props.data.cities)}{' '}
+                  <span className="text-gray-500 font-weight-light">
+                    cities
+                  </span>
+                </h6>
+              </div>
             </div>
-            <div className="col-auto" style={ {marginRight: 35}}>
-              <img src={IconTreasure} alt={props.data.treasures}/>
-              <h6 className="mt-3">{FormatNumber(props.data.treasures)} <span className="text-gray-500 font-weight-light">treasure</span></h6>
-            </div>
-            <div className="col-auto">
-              <img src={IconCities} alt={props.data.cities}/>
-              <h6 className="mt-3">{FormatNumber(props.data.cities)} <span className="text-gray-500 font-weight-light">cities</span></h6>
+          </div>
+
+          <div className="col-6 pl-5">
+            <div style={{ width: 520, height: 410 }}>
+              <img
+                src={ImageHero}
+                alt="House"
+                className="img-fluid position-absolute"
+                style={{ margin: '-30px 0 0 -30px', zIndex: 1 }}
+              />
+              <img
+                src={ImageHeroFrame}
+                alt="House Frame"
+                className="img-fluid position-absolute"
+                style={{ margin: '0 -15px -15px 0' }}
+              />
             </div>
           </div>
         </div>
-
-        <div className="col-6 pl-5"> 
-          <div style={{ width: 520, height: 410}}> 
-            <img src={ImageHero} alt="House" className="img-fluid position-absolute" style={{ margin: "-30px 0 0 -30px", zIndex: 1}}/> 
-            <img src={ImageHeroFrame} alt="House Frame" className="img-fluid position-absolute" style={{ margin: "0 -15px -15px 0"}}/> 
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
+    </Fade>
   );
 }
