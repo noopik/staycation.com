e1f90b4c4f2a50b116414ed6b9ab866586aef072
dwinovic <nd.cahya19@gmail.com>
2021-03-04 16:26:06 +0700

    change to brand staycation

--- src/parts/iconText.js
+++ src/parts/iconText.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import Button from 'elements/Button'
+import Button from 'elements/Button';
 
 export default function IconText() {
-  return(
-    <Button className="brand-text-icon" href="" type="link"> 
-      Contact<span className='text-gray-900'> Thor</span>
+  return (
+    <Button className="brand-text-icon" href="" type="link">
+      {/* Contact<span className='text-gray-900'> Thor</span> */}
+      Stay<span className="text-gray-900">cation</span>
     </Button>
-  )
-} 
+  );
+}
